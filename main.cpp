9a21ceeea1343bb8a3dc06dd511b5bda5b2ce253
znseday <55926888+znseday@users.noreply.github.com>
2020-05-11 16:50:19 +0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -12,22 +12,23 @@ namespace fs = std::experimental::filesystem;
 
 int main()
 {
-//    Evklid_minus evklid_minus;
-//    Evklid_mod evklid_mod;
-//    Evklid_bits evklid_bits;
-//    Tester testerEvklid(&evklid_minus, "Evklid_minus", fs::current_path().string() + "\\2.GCD");
-//    testerEvklid.AddTask(&evklid_mod, "Evklid_mod");
-//    testerEvklid.AddTask(&evklid_bits, "Evklid_bits");
-//    testerEvklid.RunTasks();
+    Evklid_minus evklid_minus;
+    Evklid_mod evklid_mod;
+    Evklid_bits evklid_bits;
+    Tester testerEvklid(&evklid_minus, "Evklid_minus", fs::current_path().string() + "\\2.GCD");
+    testerEvklid.AddTask(&evklid_mod, "Evklid_mod");
+    testerEvklid.AddTask(&evklid_bits, "Evklid_bits");
+    testerEvklid.RunTasks();
 
 
-//    Power_iter power_iter;
-//    Power_2_mult power_2_mult;
-//    Power_2_optim power_2_optim;
-//    Tester testerPower(&power_iter, "Power_iter", fs::current_path().string() + "\\3.Power");
-//    testerPower.AddTask(&power_2_mult, "Power_2_mult");
-//    testerPower.AddTask(&power_2_optim, "Power_2_optim");
-//    testerPower.RunTasks();
+    Power_iter power_iter;
+    Power_2_mult power_2_mult;
+    Power_2_optim power_2_optim;
+    Tester testerPower(&power_iter, "Power_iter", fs::current_path().string() + "\\3.Power");
+    testerPower.AddTask(&power_2_mult, "Power_2_mult");
+    testerPower.AddTask(&power_2_optim, "Power_2_optim");
+    testerPower.RunTasks();
+
 
     Fibonacci_rec fibonacci_rec;
     Fibonacci_iter fibonacci_iter;
